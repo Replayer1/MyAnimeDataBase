9f5fe1424b0e1d039d78e982052795750b39e2ac
Nutakoe <replayer702@gmail.com>
2026-02-09 22:39:53 +0300

    картинки

--- app/render.js
+++ app/render.js
@@ -5,7 +5,7 @@ function createAinmeCard(anime) {
         card.className = "anime-card";
         card.innerHTML = `
             <div class="anime-card__image-wrapper">
-                <img src=" # " alt="${anime.target_title_ru}" class="anime-card__image"/>
+                <img src="https://shikimori.one/system/animes/original/${anime.target_id}.jpg" alt="${anime.target_title_ru}" class="anime-card__image"/>
                 <div class="anime-card__rating">⭐ ${anime.score}</div>
             </div>
             <div class="anime-card__info">
@@ -14,10 +14,38 @@ function createAinmeCard(anime) {
                     <p class="anime-card__description"></p>
             </div>
             <div class="anime-card__actions">
-                <button class="btn btn--detail" onclick="openDetail(https://shiki.one/animes/'${anime.id}')">Подробнее</button>
-                <button class="btn btn--watch" onclick="watchAnime('${anime.id}')">Смотреть</button>
+                <button class="btn btn--detail">Подробнее</button>
+                <button class="btn btn--watch">Смотреть</button>
             </div>
         `;
+
+        const detailBtn = card.querySelector(".btn--detail");
+        const watchBtn = card.querySelector(".btn--watch");
+
+        
+        //Кнопка "подробнее"
+        detailBtn.addEventListener("click", () => {
+            function slugify(title) {
+            return title
+                .toLowerCase()                // нижний регистр
+                .normalize("NFD")             // разложение диакритики
+                .replace(/[\u0300-\u036f]/g, "") // удаляем акценты
+                .replace(/[^a-z0-9\s-]/g, "")   // убираем спецсимволы
+                .trim()                       // обрезаем пробелы по краям
+                .replace(/\s+/g, "-");        // пробелы → дефисы
+            }
+            function getShikiLink(anime) {
+                const id = anime.target_id;
+                const slug = slugify(anime.target_title);
+                return `https://shiki.one/animes/${id}-${slug}`;
+            }
+            window.open(anime.detailUrl || getShikiLink(anime), "_blank");
+        });
+
+        //Кнопка "смотреть"
+        watchBtn.addEventListener("click", () => {
+            window.open(anime.watchUrl || "#", "_blank");
+        });
         return card;
 }
 
